b4000e09285fb7d3c622f27436581ae9b5f102a2
Edney Pitta <edney_pitta@hotmail.com>
2016-07-13 23:59:27 -0300

    Formatting

--- test/TwigBot.js
+++ test/TwigBot.js
@@ -1,15 +1,15 @@
 'use strict'
 
-let expect = require('chai').expect;
+let expect = require('chai').expect
 let TwigBot = require('../src/TwigBot')
 
-describe("TwigBot", function() {
-  describe("Constructor", function() {
-    it("should throw error if token is null", function() {
+describe('TwigBot', () => {
+  describe('Constructor', () => {
+    it('should throw error if token is null', () => {
       expect(() => new TwigBot()).to.throw('Bot token must not be null or empty')
     })
 
-    it("should throw error if token is empty string", function() {
+    it('should throw error if token is empty string', () => {
       expect(() => new TwigBot('')).to.throw('Bot token must not be null or empty')
     })
   })
