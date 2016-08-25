82502135a2f2c417a0c2877862236866baf4ae0f
Edney Pitta <edney_pitta@hotmail.com>
2016-08-25 19:24:25 -0300

    Setup Mongoose and Win model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const TwigBot = require('./TwigBot')
 const MessageScheduler = require('./MessageScheduler')
 const koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGO_URI)
 
 const app = koa()
 const port = process.env.PORT || 3000
